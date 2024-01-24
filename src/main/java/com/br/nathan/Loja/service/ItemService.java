4aa606f3250d97457de5a5f0131cf3faa20f5f0f
nathan nickolas <nathannickolas13@gmail.com>
2024-01-24 19:22:35 -0300

    consertado

--- src/main/java/com/br/nathan/Loja/service/ItemService.java
+++ src/main/java/com/br/nathan/Loja/service/ItemService.java
@@ -3,19 +3,27 @@ package com.br.nathan.Loja.service;
 import com.br.nathan.Loja.Repository.ItemRepository;
 import com.br.nathan.Loja.model.Carrinho;
 import com.br.nathan.Loja.model.Item;
+import com.br.nathan.Loja.service.exception.ItemDuplicado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ItemService {
     @Autowired
     private ItemRepository itemRepository;
 
-    public Item saveItens(Item item) {
-        itemRepository.save(item);
-        return item;
+    public Item saveItens(Item itens) {
+        List<Item> duplicado = itemRepository.findAll();
+        for (Item item : duplicado) {
+            if (Objects.equals(itens.getNome(), item.getNome())) {
+                throw new ItemDuplicado("objeto duplicado e dseu id é " + item.getId());
+            }
+        }
+        itemRepository.save(itens);
+        return itens;
     }
 
     public List<Item> buscasItensbyIds(List<Long> itensid) {
